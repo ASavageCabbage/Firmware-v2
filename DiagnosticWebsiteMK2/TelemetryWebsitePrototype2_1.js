9b8d1133f6750a35e34ea41092323a3ec49e87be
ZEROGRAVITAS\David Widjaja <elementalis.david@gmail.com>
2017-10-14 12:27:03 -0700

    Improved code readability. Will add javascript for everything else when the MPPT and Battery schematics come out.

--- DiagnosticWebsiteMK2/TelemetryWebsitePrototype2_1.js
+++ DiagnosticWebsiteMK2/TelemetryWebsitePrototype2_1.js
@@ -1,3 +1,11 @@
+var PeakMainCurrent = 0;							//GLOBAL BECAUSE I AM TOO LAZY TO USE REAL FUNCTIONS
+var PeakMainTemperature = 0;
+var PeakMainTemperatureTime;
+var PeakMainCurrentTime;
+var StartTime = Date.now();
+			
+
+
 		function Placeholder() {
 				var Rand;
 				Rand = Math.floor(Math.random()*100);
@@ -29,15 +37,18 @@
 		function DisplayBlockC() {					//DISPLAY BLOCK FOR MAIN CURRENT
 		
 				var X = Placeholder();
-				//var Largest = 0;
-				var LargestTime;
+				//var PeakMainCurrentTime;				
+				
 				document.getElementById('MainCurrent').innerHTML = "Current: " + X;
 				if (X > PeakMainCurrent){
 					PeakMainCurrent = X;
+					PeakMainCurrentTime = Date.now() - StartTime;
+					PeakMainCurrentTime = Math.round(PeakMainCurrentTime/1000);
+					
 				}
 				document.getElementById('PeakCurrent').innerHTML = "Peak: " + PeakMainCurrent;
-				//Also get LargestTime, but I haven't fiqure out how to do that yet.
-				document.getElementById('PeakCurrentTime').innerHTML = "Peak Time: " + 0;
+				document.getElementById('PeakCurrentTime').innerHTML = "Peak Time: " + PeakMainCurrentTime + 's';
+				
 				var t = setTimeout(DisplayBlockC, 1000);	
 				
 				return PeakMainCurrent;
@@ -46,32 +57,41 @@
 		function DisplayBlockD() {					//DISPLAY BLOCK FOR MAIN TEMPERATURE
 		
 				var X = Placeholder();
-				//var Largest = 0;
-				var LargestTime;
+				//var PeakMainTemperatureTime;
+
+
+
+
 				document.getElementById('MainTemperature').innerHTML = "Temperature: " + X;
 				if (X > PeakMainTemperature){
 					PeakMainTemperature = X;
+					PeakMainTemperatureTime = Date.now() - StartTime;
+					PeakMainTemperatureTime = Math.round(PeakMainTemperatureTime/1000);
 				}
 				document.getElementById('PeakTemperature').innerHTML = "Peak: " + PeakMainTemperature;
-				//Also get LargestTime, but I haven't fiqure out how to do that yet.
-				document.getElementById('PeakTemperatureTime').innerHTML = "Peak Time: " + 0;
+
+				document.getElementById('PeakTemperatureTime').innerHTML = "Peak Time: " + PeakMainTemperatureTime + 's';
 				
 				var t = setTimeout(DisplayBlockD, 1000);	
 		}	
 
 		function ResetMainCurrent() {				//RESET PEAKMAINCURRENT AND PEAKMAINCURRENTTIME TO 0
 			PeakMainCurrent = 0;
+			PeakMainCurrentTime = 0;
 			document.getElementById('PeakCurrent').innerHTML = "Peak: " + PeakMainCurrent;
+			document.getElementById('PeakCurrentTime').innerHTML = "Peak Time: " + PeakMainCurrentTime + 's';
+			StartTime = Date.now();
 		}
 		
 		function ResetMainTemperature() {			//RESET PEAKMAINTEMPERATURE AND PEAKMAINTEMPERATURETIME TO 0
 			PeakMainTemperature = 0;		
+			PeakMainTemperatureTime = 0;
 			document.getElementById('PeakTemperature').innerHTML = "Peak: " + PeakMainTemperature;
+			document.getElementById('PeakTemperatureTime').innerHTML= "Peak Time: " + PeakMainTemperatureTime + 's';
+			StartTime = Date.now();
 		}
 		
-var PeakMainCurrent = 0;							//GLOBAL BECAUSE I AM TOO LAZY TO USE REAL FUNCTIONS
-var PeakMainTemperature = 0;
-			
+
 function MainUpdater() {
 		
 		DisplayBlockA();
